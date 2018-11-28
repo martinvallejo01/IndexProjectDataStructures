0cf8a5b0d43a918874c47a2b0109df5eae98edc4
Martin Freire <martin.vallejo01@gmail.com>
2018-11-28 09:52:23 -0500

    Creates file manager class and creates contructor

---
+++ src/sample/FilesManager.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class FilesManager {
+
+    public Scanner fileToIndexReader;
+    public Scanner fileWithExceptionsReader;
+
+    public FilesManager(String fileToIndexPath, String fileWithExceptionsPath)
+    {
+        try {
+            fileToIndexReader = new Scanner(new BufferedReader(new FileReader(fileToIndexPath)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            fileWithExceptionsReader = new Scanner(new BufferedReader(new FileReader(fileWithExceptionsPath)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Object WordsMatrix()
+    {
+        return 1;
+    }
+}
